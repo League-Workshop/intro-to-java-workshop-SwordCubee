a507254ea887b6e2061370e7fdae687fc5116c26
root <root@82a2f8fb55dc>
2020-07-28 21:54:55 +0000

    day 2

--- src/section1/Tash Me/Tash_Me/Tash_Me.pde
+++ src/section1/Tash Me/Tash_Me/Tash_Me.pde
@@ -1,8 +1,13 @@
-
+PImage mustache;
+PImage friend;
 void setup() {
-
+friend = loadImage("friend.jpeg");
+size(800,600);
+friend.resize(width,height);
+mustache = loadImage("mustache.png");
 }
 
 void draw() {
-
+background(friend);
+image(mustache, 200, 200);
 }
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -5,11 +5,19 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		// START HERE
+	Robot rob = new Robot();
 	
-		
-		
+	rob.setSpeed(100);
+	rob.setPenColor(Color.black);
+	rob.setPenWidth(10);
+	rob.penDown();
+	for (int i = 0; i < 100; i++) {
+		rob.move(1)
+		rob.turn(1);
+	}
+	rob.penUp();
 	}
 }
