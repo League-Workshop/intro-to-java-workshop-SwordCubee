1c0a726663447258190f3cd365ad4bbfd4de108f
root <root@1adcd5528e49>
2020-07-30 21:57:11 +0000

    OKay

--- src/section2/PentagonCrazy.java
+++ src/section2/PentagonCrazy.java
@@ -16,23 +16,22 @@ rob.penDown();
 		// 8. Make the robot go at maximum speed (100)
 rob.setSpeed(100);
 		// 9. Set the pen to a color that you like for the shape
-rob.setPenColor(color.orange);
+rob.setPenColor(Color.orange);
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-
+int sides = 5;
 		// 5. Make a second variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6
-
+int angle = 360/sides;
 		// 7. Use a for loop to repeat the steps below (#2, #10, #6, #11) 200 times. When this is done you should see a pentagon
-
+for (int i = 0; i < 200; i++) {
 			// 2. Move the robot 200 pixels
-	rob.move(200);
+	rob.move(i);
 			// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
-	
 			// 6. Turn the robot the amount in your angle variable
-	
+	rob.turn(angle);
 			// 11. Turn the robot one more degree
-
+	rob.turn(1);
 	}
-
+	}
 	// Variations:
 	// *12. make the pattern really huge
 	// *13. randomize the color of the pattern 
--- src/section2/SmartShapes.java
+++ src/section2/SmartShapes.java
@@ -9,19 +9,22 @@ public class SmartShapes {
 Robot rob = new Robot();
 // 3. Put the robot's pen down
 rob.penDown();
-rob.turn(45);
+rob.turn(320);
         // 6. Make the robot move as fast as possible
 rob.setSpeed(100);
 
         // 5. Use a for loop to repeat everything below 4 times. 
-for (int i = 0; i < 3; i++) {
+int triangle = 3;
+int pentagon = 5;
+int decagon = 10;
+for (int i = 0; i < decagon; i++) {
 
         //         2. Move your robot 200 pixels
 
-	rob.move(200);
+	rob.move(50);
 
         //         4. Turn the robot 90 degrees to the right
-rob.turn(120);	
+rob.turn(360/decagon);	
     	
     		// 7. Change steps 4 and 5 to draw a different shape (e.g. triangle (3-sides), pentagon (5-sides), decagon (10-sides)).
    
